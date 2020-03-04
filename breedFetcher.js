0d172a73b00a78121a64d66cb771546f60606a77
Aurlin Dhillon <aurlindhillon@gmail.com>
2020-03-03 22:59:32 -0800

    made API request to get cat info

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require("request");
+
+const fetchBreedDescription = (argument, callback) => {
+  const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + argument;
+  request(URL, (error, response, body) => {
+    if (error) {
+      callback("Something seems to be wrong with the URL");
+    } else {
+      const data = JSON.parse(body);
+      console.log(data);
+      console.log(typeof data);
+      if (data[0] !== undefined) {
+        callback(error, data[0]["description"].trim());
+      } else {
+        callback("Seems like you didnt write a valid cat breed!");
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
